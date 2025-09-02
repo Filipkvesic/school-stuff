c24d15cfdcd2efbf50da03c31ddc47abe8fa369e
Filipkvesic <22fkvesic.acc@lwetb.ie>
2025-09-02 14:27:39 +0100

    Add files via upload

---
+++ q 6.py
@@ -0,0 +1,7 @@
+#Author filip kvesic
+#date 02 september 2025
+#making radius to area calculator
+circle_radius = input('please enter the circle radius: ')
+circle_radius = int(circle_radius)
+total = 3.1415926535897932384 * (circle_radius * circle_radius)
+print('the area of the circle is',total)
---
+++ q 7.py
@@ -0,0 +1,17 @@
+#author filip kvesic
+#date 2 september 2025
+#description average of 5 numbers
+print('please enter 5 numbers')
+first = input('first number: ')
+second = input('second number: ')
+third = input('third number: ')
+fourth = input('fourth number: ')
+fifth = input('fifth number: ')
+first = int(first)
+second = int(second)
+third = int(third)
+fourth = int(fourth)
+fifth = int(fifth)
+total = first + second + third + fourth + fifth
+total2 = total / 5
+print('the average is',total2)
