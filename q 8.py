80a571499fdc992a79a4ae920b051d486f1a6eeb
Filipkvesic <22fkvesic.acc@lwetb.ie>
2025-09-02 14:35:25 +0100

    Add files via upload

---
+++ q 8.py
@@ -0,0 +1,9 @@
+#author filip kvesic
+#date 2 september 2025
+#description height converter
+height = input('please enter your height in cm: ')
+height = int(height)
+total = height / 2.54
+total2 = height / 12
+print('you are',total,'inches tall')
+print('you are',total2,'feet tall')
