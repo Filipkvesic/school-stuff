03614c6c31e4d8620bba8d4bea602468eb3b09e7
Filipkvesic <22fkvesic.acc@lwetb.ie>
2025-09-04 20:18:25 +0100

    Add files via upload

---
+++ favourite colour thing question 4.py
@@ -0,0 +1,9 @@
+#Author: Filip Kvesic
+#Date: 01 September
+#Description: favourite colour
+print('enter your favourite colour')
+colour = input('colour: ')
+if (colour == 'red') or (colour == 'Red') or (colour == 'RED'):
+    print('i like red too')
+else: print('i dont like',colour,'i prefer red')
+    
