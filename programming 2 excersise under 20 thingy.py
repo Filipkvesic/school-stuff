a75453a1be55c2b4e1f57f88786ad05d523737f8
Filipkvesic <22fkvesic.acc@lwetb.ie>
2025-09-04 09:36:05 +0100

    Add files via upload

---
+++ programming 2 excersise under 20 thingy.py
@@ -0,0 +1,10 @@
+#Author Filip Kvesic
+#date 4 september 2025
+#description below 20
+print('please enter a number below 20')
+number = input('number: ')
+number = float(number)
+if (number<=20):
+    print('thank you')
+else:
+    print('not a valid number')
