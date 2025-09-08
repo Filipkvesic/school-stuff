6fe08d326d88d2a4d4d999eeceff6edf75dc999a
Filipkvesic <22fkvesic.acc@lwetb.ie>
2025-09-08 12:46:52 +0100

    Add files via upload

---
+++ question 4 loop thingy.py
@@ -0,0 +1,10 @@
+#author filip kvesic
+#date 8th september 2025
+#question 4 display each letter on separate line and repeat given number of times
+name = input('please enter your name: ')
+number = input('please enter your number: ')
+number = int(number)
+for name2 in name:
+    for Name in range(number):
+        print(name2)
+    
