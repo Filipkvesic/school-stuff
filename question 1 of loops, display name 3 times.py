b37b4163d1f7c7214498b85d48a2798ec816e083
Filipkvesic <22fkvesic.acc@lwetb.ie>
2025-09-08 12:31:58 +0100

    Add files via upload

---
+++ question 1 of loops, display name 3 times.py
@@ -0,0 +1,7 @@
+#author filip kvesic
+#date 8th september 2025
+#question 1 ask user name, print 3 times name
+name = input('please enter your name: ')
+for Name in range(3):
+    print(name, end=',')
+    
