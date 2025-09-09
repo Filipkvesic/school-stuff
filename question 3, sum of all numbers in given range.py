91d76c12019273747784dbed8ffac7d6342ccdaf
Filipkvesic <22fkvesic.acc@lwetb.ie>
2025-09-09 14:39:57 +0100

    Add files via upload

---
+++ question 3, sum of all numbers in given range.py
@@ -0,0 +1,16 @@
+#author filip kvesic
+#date 9th september 2025
+#question 2.programme 4 print all even numbers in given range
+number = 0
+given_range = input('please select a given range: ')
+given_range = int(given_range)
+given_range = given_range + 1
+for loop in range(given_range):
+    number = number + loop
+print(number)
+
+
+
+
+
+    
