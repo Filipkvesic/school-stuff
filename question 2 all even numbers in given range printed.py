4a38db20e08d8ec950b1eee1c8106a7098d8c063
Filipkvesic <22fkvesic.acc@lwetb.ie>
2025-09-09 14:26:26 +0100

    Add files via upload

---
+++ question 2 all even numbers in given range printed.py
@@ -0,0 +1,14 @@
+#author filip kvesic
+#date 9th september 2025
+#question 2.programme 4 print all even numbers in given range
+number = 0
+given_range = input('please select a range for the even numbers: ')
+given_range = int(given_range)
+for loop in range(given_range):
+    if (loop%2 == 0):
+        print(loop)
+
+
+
+
+    
