80c418b0e56fc5be4cd8e0b3e1085bfa660971c5
Filipkvesic <22fkvesic.acc@lwetb.ie>
2025-09-09 14:11:41 +0100

    Add files via upload

---
+++ question 1 first ten natural numbers printed.py
@@ -0,0 +1,11 @@
+#author filip kvesic
+#date 9th september 2025
+#question 1.programme 4 first 10 natural numbers
+number = 0
+for loop in range(10):
+    number = number + 1
+    print(number)
+
+
+
+    
