ba769846e893aec424941205017b8b099fab2196
Filipkvesic <22fkvesic.acc@lwetb.ie>
2025-09-11 09:01:56 +0100

    Add files via upload

---
+++ question 3 loop questions sum of all in given range.py
@@ -0,0 +1,9 @@
+#author filip kvesic
+#date 11th september 2025
+#question 3.programme 4 print sum of all numbers in given range
+number = 0
+number1 = int(input('please enter the range: '))
+
+for i in range(number1):
+    number = number + i + 1
+print(number)
