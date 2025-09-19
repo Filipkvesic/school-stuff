c8067d27ed96a0290ba5f66523368e1fd62b5688
Filipkvesic <22fkvesic.acc@lwetb.ie>
2025-09-19 09:27:49 +0100

    Add files via upload

---
+++ question 8 for the flowchart algorithm thing, area + perimeter of rectangle.py
@@ -0,0 +1,11 @@
+# the date is septemeber 19 2025
+# the author: is Filip Kvesic
+# printing the area and perimeter of a rectangle
+length = input('please enter the length of the rectangle: ')
+width = input('please enter the width of the rectangle: ')
+length = int(length)
+width = int(width)
+area = length * width
+print('the area is: ',area)
+perimeter = (length * 2) + (width * 2)
+print('the perimeter is: ',perimeter)
