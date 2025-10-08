69e4ceb08988983b6daea4c71ee627b1bbad873b
Filipkvesic <22fkvesic.acc@lwetb.ie>
2025-10-08 23:00:40 +0100

    Add files via upload

---
+++ programme 7/programme 7 question 3.py
@@ -0,0 +1,24 @@
+#author: filip kvesic
+#date: 8/10 2025
+#description: programme thats converts binary to decimal
+number = int(input('write a binary number: '))
+number1 = number
+digits = 0
+total = 0
+for i in range(1, number1 + 1):
+    if (number < 1):
+        i = number1
+    else:
+        number = number / 10
+        digits = digits + 1
+
+       
+       
+for i in range(digits, 0, -1):
+    numbers = number1 // (10**(i-1))
+    nextNumber = numbers
+    if i != digits:
+        nextNumber = nextNumber - minusNumber
+    minusNumber = numbers*(10)
+    total += (nextNumber*2)**i
+print('in decimal form this is',int(total/2))
