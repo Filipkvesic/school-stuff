3607f5f4e36554f2ba39c9eba6638286400f30e3
Filipkvesic <22fkvesic.acc@lwetb.ie>
2025-10-17 08:59:07 +0100

    Add files via upload

---
+++ programme 9 question 1.py
@@ -0,0 +1,6 @@
+#author filip kvesic
+#date 17/10/2025
+#description ask for name and give length of it
+name = input('please give me your name and ill give the length of it: ')
+Name = len(name)
+print('your name is', Name,'characters long')
