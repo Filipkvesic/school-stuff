4e93df8e2ed7406c0ee8847709497e64f9420cff
Filipkvesic <22fkvesic.acc@lwetb.ie>
2025-10-17 09:05:51 +0100

    Add files via upload

---
+++ programme 9 question 2.py
@@ -0,0 +1,9 @@
+#author filip kvesic
+#date 17/10/2025
+#description ask for name and give length of it
+firstname = input('please give me your first name : ')
+secondname = input('please give me your second name : ')
+fullname = firstname + secondname
+Name = len(fullname)
+print('your full name is', fullname, 'and it is', Name, 'characters long in total')
+
