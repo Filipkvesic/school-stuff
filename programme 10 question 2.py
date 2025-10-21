f70e7c31ddca9b7e38f8de2f83f3decf47fe95f2
Filipkvesic <22fkvesic.acc@lwetb.ie>
2025-10-21 14:36:54 +0100

    Add files via upload

---
+++ programme 10 question 2.py
@@ -0,0 +1,8 @@
+#author filip kvesic
+#date 21/10/2025
+#description calculate the length of a string using a while loop and for loop
+count = 0
+string = 'Python Refrence Guide Version one point three'
+for characters in (string):
+    count += 1
+print(count)
