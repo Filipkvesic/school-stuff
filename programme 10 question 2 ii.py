649e82a90c576f9b615ed2454441465a7f6fd7a3
Filipkvesic <22fkvesic.acc@lwetb.ie>
2025-10-23 09:06:43 +0100

    Add files via upload

---
+++ programme 10 question 2 ii.py
@@ -0,0 +1,8 @@
+#author: Filip Kvesic
+#date 23/10/2025
+#description calculate length of string using a while loop
+string = 'a the who what where why'
+i = 0
+while i < len(string):
+    i += 1
+print(i)
